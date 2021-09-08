e9c3a5afd3d3fca058b3bfb960599b8677856c19
codenet_toanlk <toanlk@codenet.jp>
2021-09-08 09:46:58 +0700

    fix(*): lambda

--- database.tf
+++ database.tf
@@ -1,27 +1,27 @@
-resource "aws_db_instance" "postgres_db" {
-  allocated_storage      = 100
-  engine                 = "postgres"
-  engine_version         = "11.10"
-  instance_class         = "db.t3.micro"
-  identifier             = "mydb1"
-  name                   = "mydb1"
-  username               = "postgres"
-  password               = "postgres"
-  db_subnet_group_name   = aws_db_subnet_group.db_sn_group.name
-  vpc_security_group_ids = [aws_security_group.postgres_sg.id]
-  publicly_accessible = true
-  skip_final_snapshot = true
+# resource "aws_db_instance" "postgres_db" {
+#   allocated_storage      = 100
+#   engine                 = "postgres"
+#   engine_version         = "11.10"
+#   instance_class         = "db.t3.micro"
+#   identifier             = "mydb1"
+#   name                   = "mydb1"
+#   username               = "postgres"
+#   password               = "postgres"
+#   db_subnet_group_name   = aws_db_subnet_group.db_sn_group.name
+#   vpc_security_group_ids = [aws_security_group.postgres_sg.id]
+#   publicly_accessible = true
+#   skip_final_snapshot = true
   
-}
+# }
 
-resource "aws_elasticache_cluster" "redis_db" {
-  cluster_id           = "redis-db"
-  engine               = "redis"
-  node_type            = "cache.t2.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
-  port                 = 6379
-  security_group_ids   = [aws_security_group.redis_sg.id]
-  subnet_group_name    = aws_elasticache_subnet_group.redis_sg.name
-}
+# resource "aws_elasticache_cluster" "redis_db" {
+#   cluster_id           = "redis-db"
+#   engine               = "redis"
+#   node_type            = "cache.t2.micro"
+#   num_cache_nodes      = 1
+#   parameter_group_name = "default.redis3.2"
+#   engine_version       = "3.2.10"
+#   port                 = 6379
+#   security_group_ids   = [aws_security_group.redis_sg.id]
+#   subnet_group_name    = aws_elasticache_subnet_group.redis_sg.name
+# }
--- lambda.tf
+++ lambda.tf
@@ -1,12 +1,3 @@
-# ECR
-# resource "aws_ecr_repository" "repository" {
-#   name = var.project_name
-
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
-# }
-
 # Lambda function
 resource "aws_iam_role" "iam_for_lambda" {
   name = "${var.project_name}_iam_for_lambda"
@@ -29,15 +20,15 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lamba_exec_role_eni" {
-  role = "${aws_iam_role.iam_for_lambda.name}"
+  role       = aws_iam_role.iam_for_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
 resource "aws_lambda_function" "lambda" {
   function_name = "${var.project_name}_api"
   role          = aws_iam_role.iam_for_lambda.arn
-  depends_on = [aws_iam_role_policy_attachment.lamba_exec_role_eni]
-  package_type = "Image"
+  depends_on    = [aws_iam_role_policy_attachment.lamba_exec_role_eni]
+  package_type  = "Image"
 
   image_uri = "069217422023.dkr.ecr.us-west-2.amazonaws.com/serverless-test:test-image"
 
@@ -72,40 +63,80 @@ resource "aws_api_gateway_method" "proxy_method" {
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id             = aws_api_gateway_rest_api.lambda_api.id
-  resource_id             = aws_api_gateway_resource.proxy.id
-  http_method             = aws_api_gateway_method.proxy_method.http_method
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
 
   integration_http_method = "ANY"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
+resource "aws_api_gateway_method_response" "response_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "response_proxy_integration" {
+   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+   resource_id = aws_api_gateway_resource.proxy.id
+   http_method = aws_api_gateway_method.proxy_method.http_method
+   status_code = "200"
+
+   response_templates = {
+     "application/json" = ""
+   } 
+}
+
 resource "aws_api_gateway_method" "proxy_root" {
-   rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
-   resource_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
-   http_method   = "ANY"
-   authorization = "NONE"
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  resource_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+
+  integration_http_method = "ANY"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "response_proxy_root" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "response_proxy_root_integration" {
    rest_api_id = aws_api_gateway_rest_api.lambda_api.id
    resource_id = aws_api_gateway_method.proxy_root.resource_id
    http_method = aws_api_gateway_method.proxy_root.http_method
+   status_code = "200"
 
-   integration_http_method = "POST"
-   type                    = "AWS_PROXY"
-   uri                     = aws_lambda_function.lambda.invoke_arn
+   response_templates = {
+     "application/json" = ""
+   } 
 }
 
 resource "aws_api_gateway_deployment" "deploy" {
-   depends_on = [
-     aws_api_gateway_integration.lambda,
-     aws_api_gateway_integration.lambda_root,
-   ]
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
+  ]
 
-   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-   stage_name  = "dev"
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  stage_name  = "dev"
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
