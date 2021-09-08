ad93dffc602ab7722c7ac35af3847be9fab7738e
codenet_toanlk <toanlk@codenet.jp>
2021-09-08 16:11:14 +0700

    fix(*): lambda

--- lambda.tf
+++ lambda.tf
@@ -71,20 +71,21 @@ resource "aws_api_gateway_integration" "lambda" {
   resource_id = aws_api_gateway_resource.proxy.id
   http_method = aws_api_gateway_method.proxy_method.http_method
 
-  integration_http_method = "ANY"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
 resource "aws_api_gateway_integration_response" "response_proxy_integration" {
-   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-   resource_id = aws_api_gateway_resource.proxy.id
-   http_method = aws_api_gateway_method.proxy_method.http_method
-   status_code = "200"
+  depends_on  = [aws_api_gateway_integration.lambda]
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+  status_code = "200"
 
-   response_templates = {
-     "application/json" = ""
-   } 
+  response_templates = {
+    "application/json" = ""
+  }
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
@@ -99,7 +100,7 @@ resource "aws_api_gateway_integration" "lambda_root" {
   resource_id = aws_api_gateway_method.proxy_root.resource_id
   http_method = aws_api_gateway_method.proxy_root.http_method
 
-  integration_http_method = "ANY"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
@@ -116,14 +117,15 @@ resource "aws_api_gateway_method_response" "response_proxy_root" {
 }
 
 resource "aws_api_gateway_integration_response" "response_proxy_root_integration" {
-   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-   resource_id = aws_api_gateway_method.proxy_root.resource_id
-   http_method = aws_api_gateway_method.proxy_root.http_method
-   status_code = "200"
+  depends_on  = [aws_api_gateway_integration.lambda_root]
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+  status_code = "200"
 
-   response_templates = {
-     "application/json" = ""
-   } 
+  response_templates = {
+    "application/json" = ""
+  }
 }
 
 resource "aws_api_gateway_deployment" "deploy" {
