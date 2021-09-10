a167cf68e144d52de8e74e4f5f7daf4329db283b
codenet_toanlk <toanlk@codenet.jp>
2021-09-10 11:21:26 +0700

    fix(*): s3

--- s3.tf
+++ s3.tf
@@ -1,21 +1,19 @@
 # S3 Bucket
 resource "aws_s3_bucket" "s3_bucket_terrarom" {
   bucket = "${var.project_name}-bucket-tera"
-  acl    = "public-read"
 
   tags = {
     Name = "S3 website hosting"
   }
   policy = <<EOF
 {
-  "Version": "2008-10-17",
+  "Version": "2012-10-17",
+  "Id": "Policy1615945303409",
   "Statement": [
     {
-      "Sid": "PublicReadForGetBucketObjects",
+      "Sid": "Stmt1615945299825",
       "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
+      "Principal": "*",
       "Action": "s3:GetObject",
       "Resource": "arn:aws:s3:::${var.project_name}-bucket-tera/*"
     }
@@ -35,102 +33,102 @@ locals {
 
 
 # Clouldfront
-# resource "aws_cloudfront_distribution" "s3_distribution" {
-#   origin {
-#     domain_name = aws_s3_bucket.s3_bucket_terrarom.bucket_regional_domain_name
-#     origin_id   = local.s3_origin_id
-#   }
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.s3_bucket_terrarom.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
 
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   comment             = "Cloudfront for s3"
-#   default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Cloudfront for s3"
+  default_root_object = "index.html"
 
-#   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = "website"
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
-#     forwarded_values {
-#       query_string = false
+    forwarded_values {
+      query_string = false
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 0
-#     max_ttl                = 0
-#   }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
 
-#   ordered_cache_behavior {
-#     path_pattern     = "/"
-#     allowed_methods  = ["GET", "HEAD"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
+  ordered_cache_behavior {
+    path_pattern     = "/"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
-#     forwarded_values {
-#       query_string = false
+    forwarded_values {
+      query_string = false
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     min_ttl                = 0
-#     default_ttl            = 0
-#     max_ttl                = 0
-#     compress               = false
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+    compress               = false
+    viewer_protocol_policy = "redirect-to-https"
+  }
 
-#   ordered_cache_behavior {
-#     path_pattern     = "index.html"
-#     allowed_methods  = ["GET", "HEAD"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
+  ordered_cache_behavior {
+    path_pattern     = "index.html"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
-#     forwarded_values {
-#       query_string = false
+    forwarded_values {
+      query_string = false
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     min_ttl                = 0
-#     default_ttl            = 0
-#     max_ttl                = 0
-#     compress               = false
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+    compress               = false
+    viewer_protocol_policy = "redirect-to-https"
+  }
 
 
-#   custom_error_response {
-#     error_code            = 403
-#     response_code         = 200
-#     response_page_path = "/index.html"
-#     error_caching_min_ttl = 10
-#   }
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 10
+  }
 
-#     custom_error_response {
-#     error_code            = 404
-#     response_code         = 200
-#     response_page_path = "/index.html"
-#     error_caching_min_ttl = 10
-#   }
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 10
+  }
 
-#   price_class = "PriceClass_All"
+  price_class = "PriceClass_All"
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-# }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
